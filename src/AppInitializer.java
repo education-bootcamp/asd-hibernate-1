b82da46fee90947744eea8042f9fe8908a81db8d
ozen-hub <info.ozenhub@gmail.com>
2023-04-16 23:24:38 +0530

    find Student

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,14 +1,17 @@
 import entity.Student;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class AppInitializer {
     public static void main(String[] args) {
-        Student student = new Student(1, "Jayantha", 50);
-        saveStudent(student);
+        Student student = new Student(1, "Namal", 40);
+        //saveStudent(student);
         //findStudent(1);
+        findAllStudents();
     }
 
     private static void saveStudent(Student student) {
@@ -30,6 +33,13 @@ public class AppInitializer {
                 System.out.println("Can\'t find data");
             }
 
+        }
+    }
+    private static void findAllStudents(){
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            Query query = session.createQuery("FROM Student"); // HQL (Hibernate Query Language)
+            List<Student> students = query.list();
+            System.out.println(students);
         }
     }
 }
